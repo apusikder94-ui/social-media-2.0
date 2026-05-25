05b83eafc9868bb325ab34dc90d6093cf10ff981
Apu Sikder <apusikder750@gmail.com>
2026-05-25 12:26:44 +0600

    midlleare

--- client/my-app/middleware.ts
+++ client/my-app/middleware.ts
@@ -1,18 +1,20 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
 
-  const isProtectedRoute =
-    request.nextUrl.pathname.startsWith("/social");
+  const { pathname } = request.nextUrl;
 
-  if (isProtectedRoute && !token) {
+  const isProtected = pathname.startsWith("/social");
+  const isAuth = pathname.startsWith("/auth");
+
+  if (isProtected && !token) {
     return NextResponse.redirect(new URL("/auth/signUp", request.url));
+  }
+
+  if (isAuth && token) {
+    return NextResponse.redirect(new URL("/social", request.url));
   }
 
   return NextResponse.next();
 }
-export const config = {
-  matcher: ["/social/:path*"],
-};
