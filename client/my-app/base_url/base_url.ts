ee51e1d7c585eb258cddad12595f7caccc9db3a0
apusikder94-ui <apusikder94@gmail.com>
2026-06-11 13:58:35 +0600

    add backed url

--- client/my-app/base_url/base_url.ts
+++ client/my-app/base_url/base_url.ts
@@ -1,3 +1,1 @@
-// export const BASE_URL="https://social-media-app-full-stack-2.onrender.com"
-export const BASE_URL="http://localhost:5000"
-// export const BASE_URL="http://192.168.0.194:5000"
+export const BASE_URL="https://social-media-2-0.onrender.com"
